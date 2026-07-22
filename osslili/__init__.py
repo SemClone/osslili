eea32da071f7378f1a84d4fff9e8f203b3f144e6
Oscar V <oscarvalenzuelab@users.noreply.github.com>
2026-07-21 18:10:38 -0700

    chore: release v1.6.4
    Version bump to 1.6.4 for the license detection accuracy fixes (#76).
    
    Updated:
    - osslili/__init__.py: version 1.6.3 -> 1.6.4
    - pyproject.toml: version 1.6.3 -> 1.6.4
    - CHANGELOG.md: added v1.6.4 release notes

--- osslili/__init__.py
+++ osslili/__init__.py
@@ -13,7 +13,7 @@ if os.environ.get('OSLILI_DEBUG') != '1':
     except ImportError:
         pass
 
-__version__ = "1.6.3"
+__version__ = "1.6.4"
 
 from .core.generator import LicenseCopyrightDetector
 from .core.models import (
